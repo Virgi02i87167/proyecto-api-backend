54a7a55068d5b2b5949471019974f63759ed0d88
Virgi02i87167 <131634183+Virgi02i87167@users.noreply.github.com>
2024-05-28 12:28:12 -0600

    coommit

--- peliculasfrontend/src/App.js
+++ peliculasfrontend/src/App.js
@@ -2,45 +2,48 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const App = () => {
-  const [tasks, setTasks] = useState([]);
-  const [newTaskTitle, setNewTaskTitle] = useState('');
+  const [movies, setMovies] = useState([]);
+  const [newTitle, setnewTitle] = useState('');
+  const [newProtagonista, setnewProtagonista] = useState('');
+  const [newCategoria, setnewCategoria] = useState('');
+  const [newUrl, setnewUrl] = useState('');
 
   useEffect(() => {
-    fetchTasks();
+    fetchMovies();
   }, []);
 
-  const fetchTasks = async () => {
+  const fetchMovies = async () => {
     try {
       const response = await axios.get('http://localhost:5000/movies');
-      setTasks(response.data);
+      setMovies(response.data);
     } catch (error) {
       console.error('Error al recuperar peliculas:', error);
     }
   };
 
-  const createTask = async () => {
+  const createMovies = async () => {
     try {
-      const response = await axios.post('http://localhost:5000/movies', { title: newTaskTitle });
-      setTasks([...tasks, response.data]);
-      setNewTaskTitle('');
+      const response = await axios.post('http://localhost:5000/movies', { title: newTitle, protagonista: newProtagonista, categoria: newCategoria, url: newUrl });
+      setMovies([...movies, response.data]);
+      setnewTitle('');
     } catch (error) {
       console.error('Error al crear tareas:', error);
     }
   };
 
-  const updateTask = async (id, updatedTask) => {
+  const updateMovies = async (id, updatedMovies) => {
     try {
-      const response = await axios.patch(`http://localhost:5000/movies/${id}`, updatedTask);
-      setTasks(tasks.map(task => (task.id === id ? response.data : task)));
+      const response = await axios.patch(`http://localhost:5000/movies/${id}`, updatedMovies);
+      setMovies(movies.map(movie => (movie.id === id ? response.data : movie)));
     } catch (error) {
       console.error('Error al modificar tarea:', error);
     }
   };
 
-  const deleteTask = async (id) => {
+  const deleteMovies = async (id) => {
     try {
       await axios.delete(`http://localhost:5000/movies/${id}`);
-      setTasks(tasks.filter(task => task.id !== id));
+      setMovies(movies.filter(movie => movie.id !== id));
     } catch (error) {
       console.error('Erorr al eliminar tarea: ', error);
     }
@@ -48,15 +51,22 @@ const App = () => {
 
   return (
     <div>
-      <h1>Administrador de tareas</h1>
-      <input type="text" value={newTaskTitle} onChange={(e) => setNewTaskTitle(e.target.value)} placeholder="Nueva tarea"/>
-      
-      <button onClick={createTask}>Agregar tarea</button>
+      <h1>Administrador de peliculas</h1>
+      <h3>Titulo</h3>
+      <input type="text" value={newTitle} onChange={(e) => setnewTitle(e.target.value)} placeholder="agregar titulo"/>
+      <h3>Protagonista</h3>
+      <input type="text" value={newProtagonista} onChange={(e) => setnewProtagonista(e.target.value)} placeholder="agregar protagonista"/>
+      <h3>Categoria</h3>
+      <input type="text" value={newCategoria} onChange={(e) => setnewCategoria(e.target.value)} placeholder="agregar categoria"/>
+      <h3>Url del video</h3>
+      <input type="text" value={newUrl} onChange={(e) => setnewUrl(e.target.value)} placeholder="agregar URL de pelicula"/>
+      <br></br>
+      <button onClick={createMovies}>Agregar pelicula</button>
       <ul>
-        {tasks.map(task => (
-          <li key={task.id}>
-            <input type="text" value={task.title} onChange={(e) => updateTask(task.id, { title: e.target.value })} />
-            <button onClick={() => deleteTask(task.id)}>Eliminar</button>
+        {movies.map(movie => (
+          <li key={movie.id}>
+            <input type="text" value={movie.title} onChange={(e) => updateMovies(movie.id, { title: e.target.value })} />
+            <button onClick={() => deleteMovies(movie.id)}>Eliminar</button>
           </li>
         ))}
       </ul>
